308669069b883b7c8a5b7c5c827aa5ce99992666
etmoore <etmoore@gmail.com>
2016-02-27 11:43:10 -0700

    handle non-numerical params and decimals

--- add.js
+++ add.js
@@ -1,7 +1,13 @@
 function add(x, y) {
+  var result;
   if (typeof x !== 'number' || typeof y !== 'number') {
     throw new Error('Params must be a number.');
   }
 
-  return x + y;
+ var result = x + y;
+ if (parseInt(result) !== result) {
+   result = parseFloat(result.toFixed(1));
+ }
+
+ return result;
 }
